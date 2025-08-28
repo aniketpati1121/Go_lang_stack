0ae1c7ccf3798cf5dcfb43880130c66b7679f3a3
aniketpatil <your-aniketpatil1121@gmail.com>
2025-08-28 16:29:54 +0530

    add 1 file

---
+++ 1_hello_world/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+//fmt is a standard library package in Go that implements formatted I/O with functions analogous to C's printf and scanf. It is commonly used for printing output to the console and formatting strings.
